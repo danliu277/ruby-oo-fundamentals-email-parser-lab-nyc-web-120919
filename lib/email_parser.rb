a1e7ffb00df774a04f61fc8bf134b0fc1b4d2ad6
Daniel Liu <danliu277@gmail.com>
2019-12-10 18:22:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,15 +10,6 @@ class EmailAddressParser
   end
   
   def parse
-    emails = @emails.split(',')
-    emails = emails.map{|x| x.split(' ')}
-    emails = emails.flatten()
-    emails.each_with_index do |value, index|
-      found_index = emails.rindex(value)
-      if(found_index != index)
-        emails.delete_at(found_index)
-      end
-    end
-    return emails
+    @emails.split(/[,\s]+/).uniq
   end
 end
